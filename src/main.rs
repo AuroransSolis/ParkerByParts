cc4af68b274edeefee59dd4d05be116ee571bf53
AuroransSolis <primalucegd@gmail.com>
2018-02-17 23:32:36 -0700

    Fiddling with the best ways to fetch trips from multiple threads.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
 extern crate asdfasdf;
 use std::thread;
-use std::sync::{mpsc, Arc, Mutex};
+use std::sync::{Arc, Mutex};
 
 use asdfasdf::TripFetcher;
 
 fn main() {
-    let fetcher = TripFetcher::new(16_384, (0, 0, 0));
+    let fetcher = TripFetcher::new(32_768, (0, 0, 0));
     let fetcher = Arc::new(Mutex::new(fetcher));
     let mut threads = vec![];
     for no in 0..3 {
         let fetcher = Arc::clone(&fetcher);
-        let request_len = (no + 1) * (no + 1) * 100;
+        let request_len = 1000;
         let checker = thread::spawn(move || {
             loop {
                 let mut m_fetcher = fetcher.try_lock();
+                let mut data = Vec::new();
                 match m_fetcher {
                     Ok(ref mut t_fetcher) => {
                         if !t_fetcher.active {
@@ -24,15 +25,16 @@ fn main() {
                             println!("{}", tferr);
                             continue;
                         }
-                        let trips = trips.unwrap();
-                        println!("Thread {} got {} trips | Start: {:?}, end: {:?}", no, trips.len(), trips[0], trips[trips.len() - 1]);
-                        for trip in trips.into_iter() {
-                            if asdfasdf::test_squares(trip) {
-                                println!("Hory shet! Solution: {:?}", trip);
-                            }
-                        }
+                        data = trips.unwrap();
+                        println!("Thread {} got {} trips | Start: {:?}, end: {:?}", no, data.len(), data[0], data[data.len() - 1]);
                     },
                     _ => continue
+                }
+                drop(m_fetcher);
+                for trip in data.into_iter() {
+                    if asdfasdf::test_squares(trip) {
+                        println!("Hory shet! Solution: {:?}", trip);
+                    }
                 }
             }
             println!("Thread {} finished execution.", no);
